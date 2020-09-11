4def2f0002ceac1110b1940f474e95b6a1040daf
Ayush Mishra <ayushm017@gmail.com>
2020-09-12 02:10:43 +0530

    refactor: move specific routers to subrouters

--- internal/routes/main.go
+++ internal/routes/main.go
@@ -8,17 +8,12 @@ import (
 
 // Route attaches routes to the given router
 func Route(router *chi.Router) {
+
+	// Universal routes
 	(*router).Get("/", controller.GetIndex())
 	(*router).Get("/version", controller.GetVersion())
 
-	// Icons
-	(*router).Get("/{pack}/icons", controller.GetAllIcons)
-	(*router).Get("/{pack}/icons/pending", controller.GetPendingIcons)
-	(*router).Get("/{pack}/icons/done", controller.GetDoneIcons)
-	(*router).Get("/{pack}/icons/{component}", controller.GetIconByComponent)
-	(*router).Post("/{pack}/icons", controller.SaveIcons)
-	(*router).Get("/{pack}/icons/count", controller.GetIconCount)
-	(*router).Get("/{pack}/icons/pending/count", controller.GetPendingIconCount)
-	(*router).Get("/{pack}/icons/done/count", controller.GetDoneIconCount)
-	(*router).Put("/{pack}/icons/status", controller.UpdateStatus)
+	// Icon pack specific router
+	(*router).Mount("/{pack}", packRouter())
+
 }
