88bae719ac0073a555c8beb856ba2ca8aadf5968
Ayush Mishra <ayushm017@gmail.com>
2020-09-11 20:24:28 +0530

    chore: better log messages

--- pkg/database/icon.go
+++ pkg/database/icon.go
@@ -13,9 +13,9 @@ import (
 // GetAllIcons gets all the icons
 func (dbc *DBClient) GetAllIcons() ([]model.Icon, error) {
 	temp := []model.Icon{}
-	zap.S().Debugw("Querying the database for all icon request...")
+	zap.S().Debugw("Querying the database for all icon requests")
 	rows, err := dbc.db.Queryx("SELECT * FROM icon_requests ORDER BY id DESC")
-	zap.S().Debugw("Scanning the result...")
+	zap.S().Debugw("Scanning the result")
 	for rows.Next() {
 		var icon model.Icon
 		err = rows.StructScan(&icon)
@@ -27,16 +27,16 @@ func (dbc *DBClient) GetAllIcons() ([]model.Icon, error) {
 		return nil, err
 	}
 
-	zap.S().Debugw("Returning with the list of all icon requests...")
+	zap.S().Debugw("Returning with the list of all icon requests")
 	return temp, nil
 }
 
 // GetIconByComponent returns the matching icon
 func (dbc *DBClient) GetIconByComponent(component string) (*model.Icon, error) {
-	zap.S().Debugw("Querying the database with the given component...")
+	zap.S().Debugw("Querying the database with the given component")
 	row := dbc.db.QueryRowx("SELECT * FROM icon_requests WHERE component = $1", component)
 
-	zap.S().Debugw("Scanning the selected icon request...")
+	zap.S().Debugw("Scanning the selected icon request")
 	var icon model.Icon
 	err := row.StructScan(&icon)
 
@@ -46,14 +46,14 @@ func (dbc *DBClient) GetIconByComponent(component string) (*model.Icon, error) {
 		return nil, err
 	}
 
-	zap.S().Debugw("Returning with the selected icon request...")
+	zap.S().Debugw("Returning with the selected icon request")
 	return &icon, nil
 }
 
 // SaveIcon upserts the icon to the database and updates requester count on conflict
 // !UNUSED
 func (dbc *DBClient) SaveIcon(icon *model.Icon) (int, error) {
-	zap.S().Debugw("Upserting the given icon request into the database...")
+	zap.S().Debugw("Upserting the given icon request into the database")
 	row := dbc.db.QueryRowx(`
 		INSERT INTO icon_requests (name, component, url)
 		VALUES ($1, $2, $3)
@@ -62,7 +62,7 @@ func (dbc *DBClient) SaveIcon(icon *model.Icon) (int, error) {
 		RETURNING *
 	`, icon.Name, icon.Component, icon.URL)
 
-	zap.S().Debugw("Scanning the inserted icon request...")
+	zap.S().Debugw("Scanning the inserted icon request")
 
 	var returned model.Icon
 	err := row.StructScan(&returned)
@@ -71,13 +71,13 @@ func (dbc *DBClient) SaveIcon(icon *model.Icon) (int, error) {
 		return returned.ID, err
 	}
 
-	zap.S().Debugw("Returning with the inserted icon request ID...")
+	zap.S().Debugw("Returning with the inserted icon request ID")
 	return returned.ID, nil
 }
 
 // SaveIcons upserts the list of icons to the database and updates requester counts on conflict
 func (dbc *DBClient) SaveIcons(icons []*model.Icon) (int, error) {
-	zap.S().Debugw("Inserting the list of icons into the database...")
+	zap.S().Debugw("Inserting the list of icons into the database")
 
 	for _, icon := range icons {
 		zap.S().Debugw("Executing the query...")
@@ -95,7 +95,7 @@ func (dbc *DBClient) SaveIcons(icons []*model.Icon) (int, error) {
 
 	}
 
-	zap.S().Debugw("Returning...")
+	zap.S().Debugw("Returning with the number of icons")
 
 	// Needs fix, updated icons also returned as inserted icon count
 	return len(icons), nil
@@ -109,7 +109,7 @@ func (dbc *DBClient) GetCount() (int, error) {
 	err := row.Scan(&count)
 
 	if err != nil {
-		zap.S().Debugw("Failed to scan count...")
+		zap.S().Debugw("Failed to scan count")
 		return -1, err
 	}
 
@@ -129,9 +129,11 @@ func (dbc *DBClient) UpdateStatus(component, status string) (string, error) {
 	err := row.Scan(&newStatus)
 
 	if err != nil {
-		zap.S().Debugw("Failed to scan status...")
+		zap.S().Debugw("Failed to scan status")
 		return "", err
 	}
+
+	zap.S().Debugw("Returning with the updated status")
 
 	return newStatus, nil
 }
