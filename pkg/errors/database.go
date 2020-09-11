c462909ed7f8d186c5c91ddcd66d5ceb66bd176f
Ayush Mishra <ayushm017@gmail.com>
2020-09-12 03:40:05 +0530

    feat: add generic database error

--- pkg/errors/database.go
+++ pkg/errors/database.go
@@ -2,5 +2,8 @@ package errors
 
 import "errors"
 
-// ErrDatabaseNotFound is a not found error
+// ErrDatabase is a generic database error
+var ErrDatabase = errors.New("Database error")
+
+// ErrDatabaseNotFound is the error meant for when the database doesn't exist
 var ErrDatabaseNotFound = errors.New("Database not found")
