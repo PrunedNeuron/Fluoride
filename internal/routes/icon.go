e6e43a1328326a3a30b88de2b47acbaca688e7b7
Ayush Mishra <ayushm017@gmail.com>
2020-09-12 02:10:23 +0530

    refactor: create icon requests subrouter

---
+++ internal/routes/icon.go
@@ -0,0 +1,30 @@
+package routes
+
+import (
+	"fluoride/internal/controller"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
+
+// IconRouter routes the endpoints associated with icon requests
+func iconRouter() http.Handler {
+	router := chi.NewRouter()
+
+	router.Get("/", controller.GetAllIcons)
+	router.Post("/", controller.SaveIcons)
+
+	router.Get("/{component}", controller.GetIconByComponent)
+
+	router.Get("/count", controller.GetIconCount)
+
+	router.Get("/pending", controller.GetPendingIcons)
+	router.Get("/pending/count", controller.GetPendingIconCount)
+
+	router.Get("/done", controller.GetDoneIcons)
+	router.Get("/done/count", controller.GetDoneIconCount)
+
+	router.Put("/status", controller.UpdateStatus)
+
+	return router
+}
