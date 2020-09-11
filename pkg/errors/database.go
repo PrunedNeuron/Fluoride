78ae126f89ef531ab533163b8cb64f28945a62f4
Ayush Mishra <ayushm017@gmail.com>
2020-09-11 19:15:38 +0530

    refactor: make error message more specific

--- pkg/errors/database.go
+++ pkg/errors/database.go
@@ -3,4 +3,4 @@ package errors
 import "errors"
 
 // ErrDatabaseNotFound is a not found error
-var ErrDatabaseNotFound = errors.New("Not Found")
+var ErrDatabaseNotFound = errors.New("Database not found")
