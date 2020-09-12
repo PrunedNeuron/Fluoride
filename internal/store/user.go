8ca9fd252333705e29ab7d321513bed98e201d06
Ayush Mishra <ayushm017@gmail.com>
2020-09-12 21:48:00 +0530

    feat: create user store

---
+++ internal/store/user.go
@@ -0,0 +1,32 @@
+package store
+
+import (
+	"fluoride/internal/model"
+	"fluoride/pkg/database"
+
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+// UserStore is the repository for the generic user model.
+type UserStore interface {
+
+	// Creates a new user
+	CreateUser(*model.User) (string, string, error)
+}
+
+// NewUserStore creates and returns a new user store instance
+func NewUserStore() UserStore {
+	var userStore UserStore
+	var err error
+
+	switch viper.GetString("storage.type") {
+	case "postgres":
+		userStore, err = database.New()
+	}
+
+	if err != nil {
+		zap.S().Fatalw("Database error", "error", err)
+	}
+	return userStore
+}
