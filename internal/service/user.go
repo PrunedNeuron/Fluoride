04c7353d24a636d5d1fac74fcf7d01e7fd5e08c9
Ayush Mishra <ayushm017@gmail.com>
2020-09-12 21:47:49 +0530

    feat: create user service

---
+++ internal/service/user.go
@@ -0,0 +1,30 @@
+package service
+
+import (
+	"fluoride/internal/model"
+	"fluoride/internal/store"
+
+	"go.uber.org/zap"
+)
+
+// UserService provides access to services related to users
+type UserService struct {
+	logger    *zap.SugaredLogger
+	userStore store.UserStore
+}
+
+// NewUserService creates and returns a new user service instance
+func NewUserService(userStore store.UserStore) *UserService {
+	service := &UserService{
+		logger:    zap.S().With("package", "service"),
+		userStore: userStore,
+	}
+
+	return service
+
+}
+
+// CreateUser creates a new user
+func (service *UserService) CreateUser(user *model.User) (string, string, error) {
+	return service.userStore.CreateUser(user)
+}
