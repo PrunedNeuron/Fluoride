fb7ac45876a4d54c090f1b4a0fce24002f7cae0c
Ayush Mishra <ayushm017@gmail.com>
2020-09-21 02:42:35 +0530

    build: add scripts

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 POSTGRES="psql --username ${POSTGRES_USER} -d ${POSTGRES_DB}"
 
-echo "Creating schema..."
+echo -e "\e[33m$(date +"%c") \e[39mCreating schema..."
 psql -d ${POSTGRES_DB} -a -U${POSTGRES_USER} -f /schema.sql
 
-echo "Creating tables..."
+echo -e "\e[33m$(date +"%c") \e[39mCreating tables..."
 psql -d ${POSTGRES_DB} -a -U${POSTGRES_USER} -f /tables.sql
