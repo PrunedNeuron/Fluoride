54358c9b0049668ec763757d025e538f315afc1b
Ayush Mishra <ayushm017@gmail.com>
2020-09-22 03:10:15 +0530

    build: add script to set env variables

---
+++ scripts/setup_env.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+# set application env to development if unset
+if [ -z "${APPLICATION_ENV}" ]; then
+	export APPLICATION_ENV=development
+fi
+
+# enable buildkit if running docker commands
+if [[ "$1" =~ "docker" || "$1" =~ "compose" ]]; then
+	echo -e "\e[33m$(date +"%c")\t\e[39mEnabling BuildKit before running docker commands"
+	export COMPOSE_DOCKER_CLI_BUILD=1
+	export DOCKER_BUILDKIT=1
+fi
+
+# set target os and arch
+echo -e "\e[33m$(date +"%c")\t\e[39mSetting OS and ARCH env variables using the \e[92m\`go env\`\e[39m command"
+export TARGETOS=$(go env GOOS)
+export TARGETARCH=$(go env GOARCH)
