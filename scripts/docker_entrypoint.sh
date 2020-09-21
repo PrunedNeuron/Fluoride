7fabcf52731a0a2a33eff37cba794b9ffb97ff4a
Ayush Mishra <ayushm017@gmail.com>
2020-09-22 03:09:33 +0530

    refactor: separate columns with tabs

--- scripts/docker_entrypoint.sh
+++ scripts/docker_entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 # If the intention is to serve, make sure postgres is ready
 # $1 = subcommand to be passed as an arg to the binary
 if [ "$#" -eq 1 ] && [ "$1" = "serve" ]; then
-	echo -e "\e[33m$(date +"%c") \e[39mInitializing database"
+	echo -e "\e[33m$(date +"%c")\t\e[39mInitializing database"
 	sh scripts/wait_for_postgres.sh
 fi
 
-echo -e "\e[33m$(date +"%c") \e[92mLaunching application"
+echo -e "\e[33m$(date +"%c")\t\e[92mLaunching application"
 bin/"${PWD##*/}" "$@"
 
 exec "$@"
