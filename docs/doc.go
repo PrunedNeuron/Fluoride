78ff39ee0e98ec488f52f65b29c17c5a4fdf6164
Ayush Mishra <ayushm017@gmail.com>
2020-09-27 14:55:30 +0530

    docs: add go swagger docs

---
+++ docs/doc.go
@@ -0,0 +1,28 @@
+// Fluoride, a robust icon pack management service.
+//
+// This is the API documentation page for the backend API used by the fluoride icon pack management service. Feel free to try hitting the endpoints using the interactive interface provided by redoc.
+//
+// Terms Of Service: there are no TOS at this moment, use at your own risk we take no responsibility
+//     Schemes: http, https
+//     Host: localhost:3000
+//     BasePath:
+//     Version: 0.0.1
+//     License: GPL 3 https://opensource.org/licenses/gpl-3.0
+//     Contact: Ayush Mishra<am@ayushm.dev> http://ayushm.dev
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Security:
+//     - api_key:
+//
+//     SecurityDefinitions:
+//         api_key:
+//            type: apiKey
+//            name: X-API-KEY
+//            in: header
+//	swagger:meta
+package docs
